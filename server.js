74da75958dc1c0cc230cb0d08c4b57ec21036aa2
whitegit318 <whiteegit89@gmail.com>
2026-04-14 19:12:28 +0530

    cros fix

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://ecolaptoprent.onrender.com"],
     credentials: true
 }));
 
